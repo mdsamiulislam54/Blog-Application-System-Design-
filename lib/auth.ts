957830ef4cc55c2a936e4d4c18d3bb396f799484
mdsamiulislam54 <samiulm5332@gmail.com>
2025-12-31 20:37:42 +0600

    (feat) email verification implement

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,37 +1,73 @@
 import { betterAuth } from "better-auth";
+import nodemailer from "nodemailer";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
-import { prisma } from "./prisma";
- console.log("BETTER_AUTH_URL:", process.env.BETTER_AUTH_URL)
+import { prisma } from "./prisma"; console.log("BETTER_AUTH_URL:", process.env.BETTER_AUTH_URL);
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Use true for port 465, false for port 587
+    auth: {
+        user: "samiulm5332@gmail.com",
+        pass: "fpvd fdhr yhtc iebt"
+    },
+});
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
     // trustedOrigins:[process.env.BETTER_AUTH_URL as string],
-   
 
-    user:{
-        additionalFields:{
-            role:{
+
+    user: {
+        additionalFields: {
+            role: {
                 type: "string",
                 required: false,
                 defaultValue: "USER"
             },
 
-            phone : {
-                type:"string",
+            phone: {
+                type: "string",
                 required: false
 
             },
 
-            status : {
+            status: {
                 type: "string",
-                defaultValue:"ACTIVE",
+                defaultValue: "ACTIVE",
                 required: false
             }
         }
     },
     emailAndPassword: {
         enabled: true,
-    }
+        autoSignIn: false,
+        requireEmailVerification: true
+    },
+    emailVerification: {
+        sendVerificationEmail: async ({ user, url, token }, request) => {
+            const verificationUrl = `${process.env.APP_URL}/email-verify?token=${token}`
+            const info = await transporter.sendMail({
+                from: 'Prisma blogs " <maddison53@ethereal.email>',
+                to: user.email,
+                subject: "Verify your email address",
+ 
+                text: `Hello ${user.name}`, // Plain-text version of the message
+                html: `
+                    <p>Hello ${user.name || "there"},</p>
+                    <p>Please verify your email address by clicking the link below:</p>
+                    <p><a href="${verificationUrl}">Verify Email</a></p>
+                    <p>If you didn’t create this account, you can ignore this email.</p>
+                    <p>— Prisma Blogs Team</p>
+                `,
+            });
+
+            console.log("Message sent:", info.messageId);
+
+        },
+    },
+
+
+
 });
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express, { Application } from "express";
+import dotenv from 'dotenv'
 import { postRouter } from "./modules/post/post.router";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "../lib/auth";
-
-
+import cors from 'cors'
+dotenv.config();
 const app:Application = express();
 app.use(express.json())
+
+app.use(cors({
+    origin:process.env.BETTER_AUTH_URL || "http://localhost:5000",
+    credentials: true
+}))
 
 app.use('/post', postRouter)
 app.all('/api/auth/*splat', toNodeHandler(auth));
